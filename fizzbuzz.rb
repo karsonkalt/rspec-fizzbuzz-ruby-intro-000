c563eaabfdaef25d7ba8f93e49292e9f7b42f5df
Karson Kalt <kars.kalt@gmail.com>
2020-08-07 01:54:11 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,14 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+    if int % 3 == 0 # if the number int is divisible by 3
+      "Fizz" # Go fizz
+    end
+  elsif int % 5 == 0
+    "Buzz"
   end
+  else
+    int
+  end
+    
 end
